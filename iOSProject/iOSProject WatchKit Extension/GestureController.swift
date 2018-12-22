580034101d335f8cbf392a627e57d4375f552607
TRAING Serey <t.sereypich@gmail.com>
2018-12-22 20:48:18 +0100

    add color bar on watch

--- iOSProject/iOSProject WatchKit Extension/GestureController.swift
+++ iOSProject/iOSProject WatchKit Extension/GestureController.swift
@@ -47,6 +47,8 @@ class GestureController: WKInterfaceController {
     }
     
     func sendValue(value: Int) {
+        let val = Double(value) / 360
+        self.hueSlider.setColor(UIColor.init(hue: CGFloat(val), saturation: 1.0, brightness: 1.0, alpha: 1.0))
         let session = WCSession.default
         guard session.isReachable else {
             return
